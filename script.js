b2782ed152f540a0445de663730ac6a96ec0ef81
sushmita-singh <sushmitas326@gmail.com>
2022-09-05 15:31:06 +0530

    update: made app interactive using color api

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const color = {};
+
 document.getElementById('btn').addEventListener('click', function() {
-   const seedColor =  document.getElementById('seed-color').value;
+   const seedColor =  document.getElementById('seed-color').value
    const r = parseInt(seedColor.substr(1, 2), 16)
     const g = parseInt(seedColor.substr(3, 2), 16)
     const b = parseInt(seedColor.substr(5, 2), 16)
    const mode = document.getElementById('color-picker').value;
    const url = `https://www.thecolorapi.com/scheme?rgb=${r},${g},${b}&mode=${mode}&count=5`
    console.log(seedColor, mode, url)
-   fetch('https://www.thecolorapi.com/scheme?hex=00A6FB&mode=monochrome&count=5')
+   fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        let footerHtml = '' 
+        for(let i = 0; i < 5; i++) {
+            color[i] = data.colors[i].hex.value
+            console.log(color[i])
+            footerHtml += `
+                <div class="hex-value" id="hex-value${i}">${color[i]}</div>
+            `
+            console.log(`color${i}`)
+            let mainHtml = document.getElementById(`color${i+1}`) 
+            mainHtml.style.backgroundColor = color[i];
+        }
+        document.getElementById('footer').innerHTML = footerHtml
+    })
 })
